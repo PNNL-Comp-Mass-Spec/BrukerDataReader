e44daeb1f578265f94795e5852e34ad22dcd9a52
Gordon Slysz <gordon.slysz@pnnl.gov>
2012-10-16 20:59:13 +0000

    added nunit catagory tag

--- BrukerDataReader.UnitTests/DataReader_PerformanceTesting.cs
+++ BrukerDataReader.UnitTests/DataReader_PerformanceTesting.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 namespace BrukerDataReader.UnitTests
 {
     [TestFixture]
+    [Category("LongRunning")]
     public class DataReader_PerformanceTesting
     {
         /// <summary>
