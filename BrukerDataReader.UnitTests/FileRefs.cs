b99166e546ff0c950d188ecb345fe979651ece5b
alchemistmatt <matthew.monroe@pnnl.gov>
2019-02-16 09:59:56 -0800

    Update file paths and add dictionary words

--- BrukerDataReader.UnitTests/FileRefs.cs
+++ BrukerDataReader.UnitTests/FileRefs.cs
@@ -2,20 +2,17 @@
 {
     public class FileRefs
     {
-        public static string RawDataBasePath = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles";
+        public static string Bruker_12T_FID_File1 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_Solarix12T\HVY_MSCAL_000001\HVY_MSCAL_000001.D\fid";
 
+        public static string Bruker_12T_ser_File1 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_Solarix12T\28JUN10_TNT_500nM_000002\ser";
 
-        public static string Bruker_12T_FID_File1 = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\Bruker\Bruker_Solarix12T\HVY_MSCAL_000001\HVY_MSCAL_000001.D\fid";
+        public static string Bruker_9T_ser_File1 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_9T\RSPH_Aonly_01_run1_11Oct07_Andromeda_07-09-02\ser";
 
-        public static string Bruker_12T_ser_File1 = @"\\protoapps\userdata\Slysz\DeconTools_TestFiles\Bruker\Bruker_Solarix12T\28JUN10_TNT_500nM_000002\ser";
+        public static string Bruker15TFile1 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_15T\092410_ubiquitin_AutoCID_000004\092410_ubiquitin_AutoCID_000004.d\ser";
 
-        public static string Bruker_9T_ser_File1 = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\Bruker\Bruker_9T\RSPH_Aonly_01_run1_11Oct07_Andromeda_07-09-02\ser";
+        public static string Bruker15TFile2 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_15T\2014_08_21_Beam_Valve_Closing_LCmode_Neg_15T_000001\2014_08_21_Beam_Valve_Closing_LCmode_Neg_15T_000001.d\ser";
 
-        public static string Bruker15TFile1 = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\Bruker\Bruker_15T\092410_ubiquitin_AutoCID_000004\092410_ubiquitin_AutoCID_000004.d\ser";
-
-        public static string Bruker15TFile2 = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\Bruker\Bruker_15T\2014_08_21_Beam_Valve_Closing_LCmode_Neg_15T_000001\2014_08_21_Beam_Valve_Closing_LCmode_Neg_15T_000001.d\ser";
-
-        public static string Bruker15T_FID_File1 = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\Bruker\Bruker_15T\2014_09_29_MnX_000002\2014_09_29_MnX_000002.d\fid";
+        public static string Bruker15T_FID_File1 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_15T\2014_09_29_MnX_000002\2014_09_29_MnX_000002.d\fid";
 
 
     }
