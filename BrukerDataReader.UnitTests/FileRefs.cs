03239df525f77d257ec96136968957b2f117c2a8
alchemistmatt <matthew.monroe@pnnl.gov>
2021-09-20 13:11:40 -0700

    Suppress warnings

--- BrukerDataReader.UnitTests/FileRefs.cs
+++ BrukerDataReader.UnitTests/FileRefs.cs
@@ -8,10 +8,12 @@
 
         public static string Bruker_12T_ser_File1 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_Solarix12T\28JUN10_TNT_500nM_000002\ser";
 
+        // ReSharper disable once StringLiteralTypo
         public static string Bruker_9T_ser_File1 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_9T\RSPH_Aonly_01_run1_11Oct07_Andromeda_07-09-02\ser";
 
         public static string Bruker15TFile1 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_15T\092410_ubiquitin_AutoCID_000004\092410_ubiquitin_AutoCID_000004.d\ser";
 
+        // ReSharper disable once StringLiteralTypo
         public static string Bruker15TFile2 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_15T\2014_08_21_Beam_Valve_Closing_LCmode_Neg_15T_000001\2014_08_21_Beam_Valve_Closing_LCmode_Neg_15T_000001.d\ser";
 
         public static string Bruker15T_FID_File1 = @"\\proto-2\unitTest_Files\BrukerDataReader\Bruker_15T\2014_09_29_MnX_000002\2014_09_29_MnX_000002.d\fid";
---
+++ BrukerDataReader/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Safe to ignore errors here", Scope = "member", Target = "~M:BrukerDataReader.BrukerSettingsFileReader.LoadApexAcqParameters(System.IO.FileInfo)~BrukerDataReader.GlobalParameters")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Leave parentheses as-is", Scope = "member", Target = "~M:BrukerDataReader.FourierTransform.RealFourierTransform(System.Double[]@)~System.Int32")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:BrukerDataReader.DataReader.GetIndexForMZ(System.Single,System.Int32)~System.Int32")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:BrukerDataReader.DataReader.GetMassSpectrum(System.Int32[],System.Single[]@,System.Single[]@)")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:BrukerDataReader.DataReader.GetMassSpectrumUsingSupposedlyFasterBinaryReader(System.Int32,System.Single[]@,System.Single[]@)")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:BrukerDataReader.FourierTransform.PerformFourierTransform(System.Int32,System.Double[]@,System.Int32)")]
