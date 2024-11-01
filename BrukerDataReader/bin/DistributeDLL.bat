a8a77b9398958fe9e8a6d37bf9f4b3ac26f2467f
alchemistmatt <matthew.monroe@pnnl.gov>
2024-10-31 17:36:53 -0700

    Update directory name in batch file

--- BrukerDataReader/bin/DistributeDLL.bat
+++ BrukerDataReader/bin/DistributeDLL.bat
@@ -1,18 +1,18 @@
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\bin\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\bin\DLL\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\bin\DLL\Release\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\Lib\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\UnitTests\bin\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x64\Debug\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x64\Release\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\GordonSlysz\DeconTools_IQ\Library\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\GordonSlysz\SIPPER\Library\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\GordonSlysz\IqTargetCreator\Library\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\GordonSlysz\O16O18Iq\Library\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\GordonSlysz\SimpleMSViewer\Library\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\KevinCrowell\LipidTools\lib\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\KevinCrowell\LipidTools\LipidTools\bin\x86\Debug\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\KevinCrowell\LipidTools\LipidToolsTest\bin\x86\Debug\ /Y /D
-xcopy F:\Documents\Projects\GordonSlysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\GordonSlysz\DimethylLabelingIq\Library\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\bin\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\bin\DLL\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\bin\DLL\Release\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\Lib\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\DataMining\MS_File_Info_Scanner\UnitTests\bin\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Gordon_Slysz\DeconTools_IQ\DeconConsole\bin\x64\Debug\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Gordon_Slysz\DeconTools_IQ\DeconConsole\bin\x64\Release\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Gordon_Slysz\DeconTools_IQ\Library\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Gordon_Slysz\SIPPER\Library\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Gordon_Slysz\IqTargetCreator\Library\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Gordon_Slysz\O16O18Iq\Library\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Gordon_Slysz\SimpleMSViewer\Library\ /Y /D
+rem xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Kevin_Crowell\LipidTools\lib\ /Y /D
+rem xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Kevin_Crowell\LipidTools\LipidTools\bin\x86\Debug\ /Y /D
+rem xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Kevin_Crowell\LipidTools\LipidToolsTest\bin\x86\Debug\ /Y /D
+xcopy F:\Documents\Projects\Gordon_Slysz\BrukerDataReader\BrukerDataReader\bin\Debug\BrukerDataReader.dll  F:\documents\projects\Gordon_Slysz\DimethylLabelingIq\Library\ /Y /D
 
 pause
